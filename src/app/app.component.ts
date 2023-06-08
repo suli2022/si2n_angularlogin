786f9a5dcab826fd5ad1e5add5cc5b157fe0c876
Nagy Janos <nagy@zold.lan>
2023-06-08 10:47:01 +0200

    Készt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login';
+  user!: string;
+  pass!: string;
+
+  login(){
+    console.log(this.user)
+    console.log(this.pass)
+  }
 }
